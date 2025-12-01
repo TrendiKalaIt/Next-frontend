890d2636b3e5bdab6287421c8e0ded3a79a16d28
TrendiKalaIt <trendikalait@gmail.com>
2025-12-02 00:29:59 +0530

    redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "trendikala.com",
+          },
+        ],
+        destination: "https://www.trendikala.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
