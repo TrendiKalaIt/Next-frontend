feae0f6e00917511ee620d8708d3cd16a6bc6d15
TrendiKalaIt <trendikalait@gmail.com>
2025-12-19 04:04:50 +0530

    afsana link offer model

--- src/components/OfferPopup.jsx
+++ src/components/OfferPopup.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import Link from "next/link";
 
 const OfferPopup = () => {
   const [show, setShow] = useState(false);
@@ -18,11 +19,15 @@ const OfferPopup = () => {
   return (
     <div className="fixed inset-16 md:inset-0 z-50 pointer-events-none">
       <div className="offer-smooth pointer-events-auto">
-        <div className="relative cursor-pointer">
-          <div className="w-28 h-28 sm:w-32 sm:h-32 md:w-40 md:h-40 border border-black border-dashed rounded-full flex items-center justify-center animate-[spin_10s_linear_infinite] bg-gradient-to-r from-amber-900 via-yellow-800 to-amber-600
- shadow-2xl">
+        <Link
+          href="/anarkali/afsana"
+          className="relative cursor-pointer block"
+          onClick={() => setShow(false)}
+        >
+          {/* Rotating Circle */}
+          <div className="w-28 h-28 sm:w-32 sm:h-32 md:w-40 md:h-40 border border-black border-dashed rounded-full flex items-center justify-center animate-[spin_10s_linear_infinite] bg-gradient-to-r from-amber-900 via-yellow-800 to-amber-600 shadow-2xl">
             <svg
-              className="w-28 h-28 sm:w-32 sm:h-32 md:w-40 md:h-40 p-1"
+              className="w-full h-full p-1"
               viewBox="0 0 100 100"
             >
               <path
@@ -34,18 +39,19 @@ const OfferPopup = () => {
               <text>
                 <textPath
                   href="#curve"
-                  className="text-[8px] md:text-[8px] uppercase tracking-[0.2em] fill-white"
+                  className="text-[8px] uppercase tracking-[0.2em] fill-white"
                 >
-                 CONFIRM FREE GIFT • FIRST 100 ORDERS ONLY • 
+                  20% OFF Afsana • Use Coupon - AFSANA20 •
                 </textPath>
               </text>
             </svg>
           </div>
 
+          {/* Center Gift Icon */}
           <div className="absolute inset-0 flex items-center justify-center">
             <span className="text-xl sm:text-2xl md:text-3xl">🎁</span>
           </div>
-        </div>
+        </Link>
       </div>
     </div>
   );
